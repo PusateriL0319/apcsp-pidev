4ef91f0e1de33c26b0e9f572a8007ee92dd87431
PusateriL0319 <50209456+PusateriL0319@users.noreply.github.com>
2019-05-29 19:11:13 -0700

    Update student.h

--- cdemo/myproject/student.h
+++ cdemo/myproject/student.h
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-
 struct Student {
   char firstName[50];
   char lastName[50];
@@ -8,9 +5,5 @@ struct Student {
   int studentID;
 };
 
-void printStudent(struct Student* student) {
-  printf("Student:\n");
-  printf("Name: %s %s\n",student -> firstName, student -> lastName);
-  printf("Age: %d\n", student -> age);
-  printf("Student ID: %d\n", student -> studentID);
-}
+void printStudent(struct Student* student);
+
